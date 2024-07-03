01348bcb2875e1c8b542bdf73bf6b79d6e1514ce
LILDesT <145715407+LILDesT@users.noreply.github.com>
2024-07-03 15:26:48 +0300

    UPD
    fINAL COMMIT

--- Todowoo-project/Todowoo/urls.py
+++ Todowoo-project/Todowoo/urls.py
@@ -26,7 +26,11 @@ urlpatterns = [
     path('logout/', views.logoutuser, name='logoutuser'),
     #todos
     path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
     path('', views.home, name='home'),
     path('create/', views.createtodo, name='createtodo'),
     path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
 ]
--- Todowoo-project/todo/views.py
+++ Todowoo-project/todo/views.py
@@ -5,7 +5,8 @@ from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
-
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -32,6 +33,8 @@ def signupuser(request):
             })
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
         #TELL THE USERS THE PASSWORD DIDNT MATCH
+
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
@@ -46,7 +49,7 @@ def loginuser(request):
         else:
             login(request, user)
             return redirect('currenttodos')
-
+@login_required
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -59,9 +62,16 @@ def createtodo(request):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'enter less characters. Try again.'})
+
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html',{'todos':todos})
+@login_required
+
+def completedtodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html',{'todos':todos})
 
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
@@ -75,3 +85,18 @@ def viewtodo(request, todo_pk):
             return redirect('currenttodos')
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error': 'bad info'})
+
+@login_required
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+@login_required
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
